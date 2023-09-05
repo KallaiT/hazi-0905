843605c026b1625936fcc7792360a47c95bafd4e
KallaiT <119456437+KallaiT@users.noreply.github.com>
2023-09-05 09:36:25 +0200

    Add files via upload

---
+++ Hazi-0904-main/script.js
@@ -0,0 +1,9 @@
+const h1element = document.querySelector('h1')
+const h2element = document.querySelector('h2')
+h1element.addEventListener('click', function katt() {
+  alert('dsadasdadsa')
+})
+
+h2element.addEventListener('click', function katt() {
+  alert('dsadasdasdasd')
+})
